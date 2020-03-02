1fe6d5e7923916f99b932389798681c0e45ce6e6
liutao.chen <liutao.chen@chinacreator.com>
2020-03-02 08:53:40 +0800

    测试

--- src/test/java/com/clt/GraduationProjectApplicationTests.java
+++ src/test/java/com/clt/GraduationProjectApplicationTests.java
@@ -1,13 +1,53 @@
 package com.clt;
 
+import com.clt.entity.Book;
+import com.clt.entity.User;
+import com.clt.service.BookService;
+import com.clt.service.UserService;
+import io.jsonwebtoken.lang.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
-class GraduationProjectApplicationTests {
+public class GraduationProjectApplicationTests{
+
+    
+
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    BookService bookService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        User user = userService.queryById("2");
+        Assert.notNull(user);
     }
 
+    @Test
+    public void test(){
+        Book book = bookService.queryById("2");
+        Assert.notNull(book);
+    }
+
+
+    @Timeout(1)
+    @Test
+    public void test1(){
+        try {
+            Thread.sleep(1111);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test2(){
+    }
 }
